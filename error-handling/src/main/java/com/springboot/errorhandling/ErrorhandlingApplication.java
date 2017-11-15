9adbb0646371c9fea9b506ed1fac1bea08c617e4
ByrsH <bgxjsh@sina.com>
2017-11-15 17:54:17 +0800

    add error handler

---
+++ error-handling/src/main/java/com/springboot/errorhandling/ErrorhandlingApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.errorhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorhandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorhandlingApplication.class, args);
+	}
+}
---
+++ error-handling/src/test/java/com/springboot/errorhandling/ErrorhandlingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.errorhandling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ErrorhandlingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
